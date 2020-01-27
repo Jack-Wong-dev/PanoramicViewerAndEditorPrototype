3a35b2df97e46eb9bef05423653026f5c68f9c6d
Jack Wong <jackwong@Jacks-MacBook-Pro.local>
2020-01-27 17:04:18 -0500

    Initial Commit

---
+++ TestingCTPano/TestingCTPano/Controller/PanoViewController.swift
@@ -0,0 +1,135 @@
+//
+//  PanoViewController.swift
+//  TestingCTPano
+//
+//  Created by Jack Wong on 1/27/20.
+//  Copyright © 2020 Jack Wong. All rights reserved.
+//
+
+import UIKit
+
+class PanoViewController: UIViewController {
+    
+    lazy private var panoView: CTPanoramaView = {
+        
+        let pV = CTPanoramaView()
+        return pV
+    }()
+    
+    lazy private var compassView: CTPieSliceView = {
+        let cV = CTPieSliceView()
+        return cV
+    }()
+    
+    lazy private var motionButton: UIButton = {
+        let button = UIButton(type: UIButton.ButtonType.system)
+        button.backgroundColor = .black
+        button.setTitleColor(.white, for: .normal)
+        button.setTitle("Touch/Motion", for: .normal)
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(motionTypeTapped),
+                         for: .touchUpInside)
+        return button
+    }()
+    
+    private func commonInit(){
+        addSubviews()
+        setConstraints()
+        panoView.compass = compassView
+    }
+    
+    private func addSubviews(){
+        view.addSubview(panoView)
+        view.addSubview(compassView)
+        view.addSubview(motionButton)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadSphericalImage()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        commonInit()
+    }
+    
+}
+
+//MARK:- Panoramic Methods
+extension PanoViewController{
+    
+    @objc private func motionTypeTapped(){
+        if panoView.controlMethod == .touch{
+            panoView.controlMethod = .motion
+        }else{
+            panoView.controlMethod = .touch
+        }
+    }
+    
+    private func loadSphericalImage() {
+        
+        self.panoView.image = UIImage(named: "pursuit")
+        
+    }
+    
+//    @objc func panoramaTypeTapped() {
+//
+//        if panoView.panoramaType == .spherical {
+//            loadCylindricalImage()
+//        } else {
+//            loadSphericalImage()
+//        }
+//    }
+    
+    //    private func loadCylindricalImage() {
+    //        self.panoView.image = UIImage(named: "cylindrical")
+    //    }
+}
+
+
+//MARK:- Constraints
+extension PanoViewController {
+    
+    private func setConstraints() {
+        setPanoViewConstraints()
+        setCompassPieConstraints()
+        setMotionButtonConstraints()
+    }
+    
+    private func setPanoViewConstraints(){
+        self.panoView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            panoView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            self.panoView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.panoView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            self.panoView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
+        
+    }
+    
+    private func setCompassPieConstraints(){
+        compassView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            compassView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -20),
+            compassView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            compassView.heightAnchor.constraint(equalToConstant: 40),
+            compassView.widthAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+    
+    private func setMotionButtonConstraints(){
+        motionButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            motionButton.widthAnchor.constraint(equalToConstant: 100),
+            motionButton.heightAnchor.constraint(equalToConstant: 40),
+            motionButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 40),
+            motionButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20),
+        ])
+    }
+    
+    
+}
---
+++ TestingCTPano/TestingCTPano/Other/ViewController.swift
@@ -0,0 +1,33 @@
+////
+////  ViewController.swift
+////  TestingCTPano
+////
+////  Created by Jack Wong on 1/21/20.
+////  Copyright © 2020 Jack Wong. All rights reserved.
+////
+//
+//import UIKit
+//import ImageIO
+//
+//
+//class ViewController: UIViewController {
+//    
+//    let mainView = MainView()
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        
+//        view.addSubview(mainView)
+//        mainView.loadSphericalImage()
+//    }
+//    
+////    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+////        return .all
+////    }
+//    
+//    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+//        print("Landscape Mode: \(UIDevice.current.orientation.isLandscape)")
+//     
+//    }
+//}
+//
